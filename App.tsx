52fc941f6befc8f4ed161a41562bd032ca52dc12
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-02-28 14:21:16 +0800

    feat: :sparkles: add dependencies and setup tailwind

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar"
+import { Text, View } from "react-native"
+
+export default function App() {
+  return (
+    <View className="flex flex-row justify-center items-center bg-red-400 h-full">
+      <Text>Open up App.js to start working on your app!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
