c267e2beb4197820bb7641c3bf46d173dce1736c
Abdul Shabbir <abdulqshabbir@gmail.com>
2024-02-28 18:23:07 +0800

    fix: :bug: talwind css version to work with nativewind

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native"
 
 export default function App() {
   return (
-    <View className="flex flex-row justify-center items-center bg-red-400 h-full">
+    <View className="flex flex-row justify-center items-center bg-gray-400 h-full">
       <Text>Open up App.js to start working on your app!!!!</Text>
       <StatusBar style="auto" />
     </View>
